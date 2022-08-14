cc5595a14e10f1afe5f9d842149e63814e013e0a
PauloJr42 <paulosraiva@hotmail.com>
2022-08-14 18:10:47 -0300

    Abertura de projeto

---
+++ src/test/java/com/imrservicos/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imrservicos.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
